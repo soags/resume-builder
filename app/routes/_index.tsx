4e70ba4380afc0c43ee41839e68e3798b7ce61ef
soags <17847282+soags@users.noreply.github.com>
2025-05-04 01:02:16 +0900

    ファイルベースルーティング

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,4 @@
-import type { Route } from "./+types/home";
+import type { Route } from "../+types/root";
 import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
