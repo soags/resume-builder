d19a9fab319a77db66997c5e25ed387a91d74842
soags <17847282+soags@users.noreply.github.com>
2025-05-04 07:53:17 +0900

    Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,28 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig, globalIgnores } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import configPrettier from 'eslint-config-prettier'
 
 export default defineConfig([
   globalIgnores(['.react-router', 'build']),
-  { 
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], 
-    plugins: { js }, 
-    extends: ["js/recommended"] 
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
   },
-  { 
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], 
-    languageOptions: { globals: {...globals.browser, ...globals.node} },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
     settings: {
       react: {
         version: 'detect',
       },
-    }, 
+    },
   },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   pluginReact.configs.flat['jsx-runtime'],
-]);
+  configPrettier
+])
