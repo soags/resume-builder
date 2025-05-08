595c4096c5c1d2a5a222c9184ab3790a2df7fcc9
soags <17847282+soags@users.noreply.github.com>
2025-05-09 06:12:47 +0900

    Highlight Input自動更新

--- src/app/me/resumes/[resumeId]/edit/highlights/_components/HighlightList.tsx
+++ src/app/me/resumes/[resumeId]/edit/highlights/_components/HighlightList.tsx
@@ -1,11 +1,17 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { getHighlights, addHighlight, deleteHighlight } from "../actions";
+import {
+  getHighlights,
+  addHighlight,
+  deleteHighlight,
+  updateHighlight,
+} from "../actions";
 import { Highlight } from "@/generated/prisma/client";
 import { GripVerticalIcon, Trash2Icon } from "lucide-react";
 import { Input } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
+import { useDebouncedCallback } from "use-debounce";
 
 export function HighlightList({
   resumeId,
@@ -57,17 +63,49 @@ export function HighlightList({
     }
   };
 
+  const debouncedUpdate = useDebouncedCallback(
+    async (id: string, text: string) => {
+      try {
+        console.log("debouncedUpdate", id, text);
+        await updateHighlight(id, text);
+      } catch (error) {
+        console.error(
+          `[HighlightList] Error updating highlight with id=${id}:`,
+          error,
+        );
+      }
+    },
+    500,
+  );
+
+  const handleUpdate = (id: string, text: string) => {
+    const current = highlights.find((h) => h.id === id);
+    if (current?.text === text) return;
+
+    const updatedHighlights = highlights.map((highlight) =>
+      highlight.id === id ? { ...highlight, text } : highlight,
+    );
+    setHighlights(updatedHighlights);
+    debouncedUpdate(id, text);
+  };
+
   return (
     <div>
       <div className="space-y-2">
         {highlights.map((highlight) => (
           <div key={highlight.id} className="flex items-center gap-x-2">
+            {/* ドラッグハンドル */}
             <div className="hover:cursor-grab">
               <GripVerticalIcon className="h-5 w-5" />
             </div>
+
+            {/* テキスト */}
             <Input
               className="focus-visible:border-border flex-1 focus-visible:ring-0"
-              defaultValue={highlight.text}
+              value={highlight.text ?? ""}
+              onChange={(e) => {
+                handleUpdate(highlight.id, e.target.value);
+              }}
             />
 
             {/* 削除ボタン */}
@@ -89,7 +127,6 @@ export function HighlightList({
         <Button variant="outline" onClick={handleAdd}>
           追加
         </Button>
-        <input type="hidden" name="resumeId" value={resumeId} />
       </div>
     </div>
   );
